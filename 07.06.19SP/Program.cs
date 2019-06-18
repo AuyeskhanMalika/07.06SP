b58579967147cfe5bed82a246a3a35af109c3dd1
auyeskhanm <auyeskhanm@gmail.com>
2019-06-18 19:31:57 +0600

    Добавьте файлы проекта.

---
+++ 07.06.19SP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07._06._19SP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new User();
+            for (int i = 0; i < 10; i++)
+            {
+                var thread = new Thread(account.BeginTransaction);
+                thread.Start();
+            }
+            Console.ReadLine();
+        }
+    }
+}
